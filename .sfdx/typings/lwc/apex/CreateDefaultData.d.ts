5107980d10a391c9ad2e9b27382e89ec9da379ed
glytop <123397208+glytop@users.noreply.github.com>
2025-06-09 16:54:33 +0300

    upd classes and triggers

---
+++ .sfdx/typings/lwc/apex/CreateDefaultData.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/CreateDefaultData.isDataCreated" {
+  export default function isDataCreated(): Promise<any>;
+}
+declare module "@salesforce/apex/CreateDefaultData.createDefaultData" {
+  export default function createDefaultData(): Promise<any>;
+}
